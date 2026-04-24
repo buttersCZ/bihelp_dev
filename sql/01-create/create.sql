73b02088cfd15ebf8e53bfd7f14ea253425cc2be
David Macak <macak521@gmail.com>
2026-04-24 22:03:02 +0200

    prvni struktura

---
+++ sql/01-create/create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS `bqtesting-494319.test_dataset.products` (
+  id INT64,
+  name STRING
+);
---
+++ sql/02-insert/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO `bqtesting-494319.test_dataset.products` (id, name)
+VALUES (1, 'Beer'), (2, 'Wine');
