3bdac99c5b15ba4f5d7fd9337f4407d37d261787
David Macak <macak521@gmail.com>
2026-04-26 07:24:10 +0200

    entry point

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "bihelp=bihelp.bihelp:main",
+            "bihelp_bq=bihelp.bihelp:main",
         ],
     },
     python_requires=">=3.10",
